5e99ffe9dcdbc6300116444d9e57d3489398fbbe
mz2 <matias.piipari@gmail.com>
2015-04-26 22:32:12 +0100

    Adds an OSX target

---
+++ VKImageOSX/VKImageOSX.h
@@ -0,0 +1,19 @@
+//
+//  VKImageOSX.h
+//  VKImageOSX
+//
+//  Created by Matias Piipari on 19/04/2015.
+//  Copyright (c) 2015 Vyacheslav Kim. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for VKImageOSX.
+FOUNDATION_EXPORT double VKImageOSXVersionNumber;
+
+//! Project version string for VKImageOSX.
+FOUNDATION_EXPORT const unsigned char VKImageOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VKImageOSX/PublicHeader.h>
+
+
